8cba03e2a1ba46a38709891043d68ea4d21e855e
okoli Chimarume <juskie115@gmail.com>
2025-01-25 16:22:12 +0100

    feat: implements search

--- js/modules/cards.js
+++ js/modules/cards.js
@@ -1,19 +1,49 @@
 function renderCards(movieList) {
-    const cardGrid = document.getElementById('card-grid');
-    cardGrid.innerHTML = '';
+  const cardGrid = document.getElementById("card-grid");
+  const searchBox = document.getElementById("search-box");
+  cardGrid.innerHTML = "";
 
-    movieList.forEach(movie => {
-        const card = document.createElement('div');
-        card.className = 'card';
-        
-        card.innerHTML = `
+  searchBox.addEventListener("input", () => {
+    const searchValue = searchBox.value.toLowerCase();
+
+    const filteredMovies = movieList.filter((movie) =>
+      movie.name.toLowerCase().includes(searchValue)
+    );
+
+    cardGrid.innerHTML = "";
+
+    filteredMovies.forEach((movie) => {
+      const card = document.createElement("div");
+      card.className = "card";
+
+      card.innerHTML = `
+                <img src="${movie.image}" alt="${movie.name}">
+                <h3>${movie.name}</h3>
+                <h4>${movie.year}</h4>
+                <h4><i>${movie.genre}</i></h4>
+                <span class="type">${movie.description}</span>
+            `;
+
+      cardGrid.appendChild(card);
+    });
+
+    if (filteredMovies.length === 0) {
+      cardGrid.innerHTML = `<p>No movies found matching your search.</p>`;
+    }
+  });
+
+  movieList.forEach((movie) => {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    card.innerHTML = `
             <img src="${movie.image}" alt="${movie.name}">
             <h3>${movie.name}</h3>
             <h4>${movie.year}</h4>
             <h4><i>${movie.genre}</i></h4>
             <span class="type">${movie.description}</span>
         `;
-        
-        cardGrid.appendChild(card);
-    });
+
+    cardGrid.appendChild(card);
+  });
 }
